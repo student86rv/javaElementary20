be7962d21918b8f20c9b605291924f6bd49c66d6
Roman <student.rv86@gmail.com>
2019-04-05 16:39:18 +0300

    initial commit

---
+++ src/com/company/FileStorage.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.List;
+
+public class FileStorage implements Storage{
+
+    @Override
+    public void removeAll() {
+
+    }
+
+    @Override
+    public void removeUser(int id) {
+
+    }
+
+    @Override
+    public void removeUserByName(String name) {
+
+    }
+
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public User getUser(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+}
